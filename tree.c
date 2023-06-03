5d17c46164e87e84e076747cbbda0245cf6962fe
Emmy <32604310+emm218@users.noreply.github.com>
2023-06-03 09:50:45 -0400

    implemented red black insert
    still untested, but now I just have to implement removal

--- tree.c
+++ tree.c
@@ -25,32 +25,160 @@
 
 #include "tree.h"
 
+// gives color from an rbe pointer, NULL is considered black.
+#define RBE_COLOR(rbe) (rbe ? rbe->color : BLACK)
+
+static struct rb_entry *rbe_rotate_left(struct rb_entry *);
+static struct rb_entry *rbe_rotate_right(struct rb_entry *);
+
 static inline struct rb_entry *
 rb_n2e(const struct rb_type *t, const void *node)
 {
-	return ((struct rb_entry *)(node + t->t_offset));
+	return ((struct rb_entry *)(node + t->offset));
 }
 
 static inline void *
 rb_e2n(const struct rb_type *t, const struct rb_entry *rbe)
 {
-	return ((void *)rbe) - t->t_offset;
+	return ((void *)rbe) - t->offset;
 }
 
+/*
+ * _rb_insert -- inserts an element elm into the tree rbt. if an element with
+ * the same key is already in the tree it will be returned, otherwise NULL.
+ */
 void *
 _rb_insert(const struct rb_type *t, rb_tree *rbt, void *elm)
 {
-	return NULL;
-}
-void *
-_rb_remove(const struct rb_type *t, rb_tree *rbt, void *elm)
-{
+	struct rb_entry *rbe = rb_n2e(t, elm);
+	struct rb_entry *tmp, *parent, *uncle, *gp;
+	void		*node;
+	int		 cmp;
+	enum { LEFT, RIGHT } ndir, pdir;
+
+	tmp = *rbt;
+
+	while (tmp != NULL) {
+		parent = tmp;
+		node = rb_e2n(t, tmp);
+
+		cmp = t->compare(elm, node);
+		if (cmp < 0)
+			tmp = tmp->left;
+		else if (cmp > 0)
+			tmp = tmp->right;
+		else
+			return node; /* the node was already here */
+	}
+
+	rbe->parent = parent;
+	rbe->left = NULL;
+	rbe->right = NULL;
+	rbe->color = RED;
+
+	if (cmp < 0) {
+		parent->left = rbe;
+		ndir = LEFT;
+	} else {
+		parent->right = rbe;
+		ndir = RIGHT;
+	}
+
+	/* now the element is in the tree but if parent is red then we have a
+	 * color violation to fix */
+rebalance:
+	/* easiest case, no color violation */
+	if (RBE_COLOR(parent) == BLACK)
+		return NULL;
+
+	gp = parent->parent;
+	/* if there's no grandparent, we can just recolor the parent */
+	if (gp == NULL) {
+		parent->color = BLACK;
+		return NULL;
+	}
+
+	if (gp->left == parent) {
+		uncle = gp->right;
+		pdir = LEFT;
+	} else {
+		uncle = gp->left;
+		pdir = RIGHT;
+	}
+
+	/*
+	 * if parent and uncle both red then we can make them black and make
+	 * grandparent red, but now grandparent might have a color violation
+	 */
+	if (RBE_COLOR(uncle) == RED) {
+		parent->color = BLACK;
+		if (uncle)
+			uncle->color = BLACK;
+		gp->color = RED;
+		parent = gp->parent;
+		goto rebalance;
+	}
+
+	/*
+	 * if uncle is black instead we need to do some rotating
+	 *
+	 * when. me when i. when u rotate
+	 */
+
+	/*
+	 * current node is an "inner" grandchild, so we need to rotate so its
+	 * not
+	 */
+	if (ndir != pdir) {
+		if (ndir == RIGHT)
+			parent = rbe_rotate_left(parent);
+		else
+			parent = rbe_rotate_right(parent);
+		ndir = pdir;
+	}
+	/* now we know its an outer grandchild */
+	if (ndir == RIGHT)
+		rbe_rotate_left(gp);
+	else
+		rbe_rotate_right(gp);
+
+	/* recolor now that everything is in the right place */
+	gp->color = RED;
+	parent->color = BLACK;
+
 	return NULL;
 }
 
 void *
-_rb_find(const struct rb_type *t, rb_tree *rbt, const void *elm)
+_rb_remove(const struct rb_type *t, rb_tree *rbt, void *elm)
 {
+	(void)t;
+	(void)rbt;
+	(void)elm;
+	return NULL;
+}
+
+/*
+ * _rb_find -- finds the node with a given key and returns it, or NULL if not
+ * in the tree
+ */
+void *
+_rb_find(const struct rb_type *t, rb_tree *rbt, const void *key)
+{
+	struct rb_entry *tmp = *rbt;
+	void		*node;
+	int		 cmp;
+
+	while (tmp) {
+		node = rb_e2n(t, tmp);
+		cmp = t->compare(key, node);
+		if (cmp < 0)
+			tmp = tmp->left;
+		else if (cmp > 0)
+			tmp = tmp->right;
+		else
+			return node;
+	}
 	return NULL;
 }
 
@@ -76,4 +204,50 @@ _rb_parent(const struct rb_type *t, const void *node)
 	struct rb_entry *rbe = rb_n2e(t, node);
 	rbe = rbe->parent;
 	return rbe == NULL ? NULL : rb_e2n(t, rbe);
+}
+
+/*
+ * rbe_rotate_left -- rotates a tree left and returns the new element
+ * occupying the place previously held by node.
+ *
+ * will fail if node has no right child to rotate into its place
+ */
+static struct rb_entry *
+rbe_rotate_left(struct rb_entry *node)
+{
+	struct rb_entry *r_child = node->right;
+
+	/* trying to rotate with a null child would break everything */
+	if (r_child == NULL)
+		return NULL;
+
+	node->right = r_child->left;
+	r_child->left = node;
+	r_child->parent = node->parent;
+	node->parent = r_child;
+
+	return r_child;
+}
+
+/*
+ * rbe_rotate_right -- rotates a tree right and returns the new element
+ * occupying the place previously held by node
+ *
+ * will fail if node has no left child to rotate into its place
+ */
+static struct rb_entry *
+rbe_rotate_right(struct rb_entry *node)
+{
+	struct rb_entry *l_child = node->left;
+
+	/* trying to rotate with a null child would break everything */
+	if (l_child == NULL)
+		return NULL;
+
+	node->left = l_child->right;
+	l_child->right = node;
+	l_child->parent = node->parent;
+	node->parent = l_child;
+
+	return l_child;
 }
